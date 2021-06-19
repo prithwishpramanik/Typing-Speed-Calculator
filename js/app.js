807185e4faa6648394999ecec51e4c53b5a7631d
Prithwish Pramanik <prithwishpramanik462@gmail.com>
2021-06-19 12:50:58 +0530

    UI improved more functionality added
    UI improved more functionality added participant details to be added

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
-var timeleft = 60;
+var timeleft = 6;
+
 document.getElementById("button").addEventListener("click", function (e){
     var x = document.getElementById("exampleFormControlTextarea1");
     var y = document.getElementById("count");
     var r = document.getElementById("result");
     var z = document.getElementById("button");
+    var g=document.getElementById("gutter");
 
     if (x.style.display === "none" && y.style.display === "none") {
         x.style.display = "block";
@@ -14,9 +16,10 @@ document.getElementById("button").addEventListener("click", function (e){
     var downloadTimer = setInterval(function(){
         if(timeleft <= 0){
             clearInterval(downloadTimer);
-            document.getElementById("count").innerHTML = "Finished";
+            document.getElementById("count").innerHTML = "Your Result";
             x.style.display = "none";
             r.style.display = "block";
+            g.style.display = "block"
 
         } else {
             document.getElementById("count").innerHTML = timeleft + " seconds remaining";
@@ -30,14 +33,41 @@ document.getElementById("button").addEventListener("click", function (e){
         let a=words.match(/(\w+)/g).length;
 
 
-        document.getElementById("result").innerHTML = a;
 
 
+        document.getElementById("result").innerHTML ="Number Of Words/second: "+a;
+
+      if(a>5 || a<=10){
+          document.getElementById("progress").style.width= "10%";
+      }else if(a>10 || a<=20){
+          document.getElementById("progress").style.width= "20%";
+      }else if(a>20 || a<=30){
+          document.getElementById("progress").style.width= "30%";
+      }else if(a>30 || a<=40) {
+          document.getElementById("progress").style.width = "40%";
+      }else if(a>40 || a<=50) {
+          document.getElementById("progress").style.width = "50%";
+      }else if(a>50 || a<=60) {
+          document.getElementById("progress").style.width = "60%";
+      }else if(a>60 || a<=70) {
+          document.getElementById("progress").style.width = "70%";
+      }
+      else if(a>70 || a<=80) {
+          document.getElementById("progress").style.width = "80%";
+      }else if(a>80 || a<=90) {
+          document.getElementById("progress").style.width = "90%";
+      }else{
+          document.getElementById("progress").style.width = "100%";
+
+      }
 
 
 
 
     }, 1000)
+
+    //document.getElementById("button").style.display="block";
+
 
 
 })
